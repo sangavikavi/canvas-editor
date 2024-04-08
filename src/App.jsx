da7e0f37bf6f759cb25b690faabf46b3a254f6fc
Sangavi <110330823+sangavikavi@users.noreply.github.com>
2024-04-08 07:38:30 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,251 @@
+import React, { useRef, useEffect, useState } from "react";
+import { TwitterPicker } from 'react-color';
+import { dummyData } from "./constants";
+import TextField from '@mui/material/TextField';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faImage } from '@fortawesome/free-solid-svg-icons';
+
+function wrapText(context, text, x, y, lineHeight, fitWidth) {
+  fitWidth = fitWidth || 0;
+  const maxCharactersPerLine = 30;
+
+  if (fitWidth <= 0) {
+    context.fillText(text, x, y);
+    return;
+  }
+
+  let words = text.split(" ");
+  let line = "";
+  words.forEach((word, index) => {
+    const testLine = (index === 0 ? "" : line + " ") + word; // Remove leading space for the first word
+    if (testLine.length > maxCharactersPerLine) {
+      context.fillText(line, x, y);
+      line = word;
+      y += lineHeight;
+    } else {
+      line = testLine;
+    }
+  });
+
+  context.fillText(line, x, y);
+}
+
+const App = () => {
+  const canvasRef = useRef();
+  const fileInputRef = useRef(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+  const [captionText, setCaptionText] = useState(dummyData.caption.text);
+  const [ctaText, setCtaText] = useState(dummyData.cta.text);
+  const [color, setColor] = useState('#0369A1'); // Initial color
+  const [lastThreeColors, setLastThreeColors] = useState([]);
+  const [showColorPicker, setShowColorPicker] = useState(false); // State to toggle color picker visibility
+
+  const handleColorChange = (selectedColor) => {
+    setColor(selectedColor.hex);
+    setLastThreeColors(prevColors => {
+      if (prevColors.length >= 3) {
+        prevColors.pop(); // Remove the oldest color
+      }
+      return [selectedColor.hex, ...prevColors]; // Add the new color to the beginning
+    });
+  };
+
+  const handleCaptionTextChange = (event) => {
+    setCaptionText(event.target.value.trim() || dummyData.caption.text);
+  };
+
+  const handleCTATextChange = (event) => {
+    setCtaText(event.target.value.trim() || dummyData.cta.text);
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const imageUrl = reader.result;
+        setPreviewUrl(imageUrl);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const triggerInputFile = () => {
+    fileInputRef.current.click();
+  };
+
+  const correctionFactors = {
+    height: 470,
+    width: 100,
+    y: 185,
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    const mask = new Image();
+    const designPattern = new Image();
+    const stroke = new Image();
+
+    mask.src = dummyData.urls.mask;
+    designPattern.src = dummyData.urls.design_pattern;
+    stroke.src = dummyData.urls.stroke;
+
+    mask.onload = () => {
+      canvas.width = mask.width;
+      canvas.height = mask.height;
+
+      context.fillStyle = color;
+      context.fillRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(mask, 0, 0);
+      context.drawImage(designPattern, 0, 0, canvas.width, canvas.height);
+
+      if (previewUrl) {
+        const image = new Image();
+        image.src = previewUrl;
+
+        image.onload = () => {
+          const scaleFactor = Math.min(
+            mask.width / image.width,
+            mask.height / image.height
+          );
+
+          const scaledWidth =
+            image.width * scaleFactor - correctionFactors.width;
+          const scaledHeight =
+            image.height * scaleFactor - correctionFactors.height;
+
+          const x = (mask.width - scaledWidth) / 2;
+          const y = (mask.height - scaledHeight) / 2 + correctionFactors.y;
+
+          context.drawImage(image, x, y, scaledWidth, scaledHeight);
+        };
+      }
+
+      const { caption } = dummyData;
+      context.font = `${caption.font_size}px Arial`;
+      context.fillStyle = caption.text_color;
+      context.textAlign = caption.alignment;
+      context.textBaseline = "top";
+      wrapText(context, captionText, caption.position.x, caption.position.y, 60, 800)
+
+      const { cta } = dummyData;
+      const ctaWidth = 270;
+      const ctaHeight = 100;
+      const ctaX = cta.position.x - ctaWidth / 2;
+      const ctaY = cta.position.y - ctaHeight / 2;
+      context.fillStyle = cta.background_color;
+      context.fillRect(ctaX, ctaY, ctaWidth, ctaHeight);
+      context.font = "35px Arial";
+      context.fillStyle = cta.text_color;
+      context.textAlign = "center";
+      context.textBaseline = "middle";
+      context.fillText(ctaText, cta.position.x, cta.position.y);
+    };
+  }, [previewUrl, captionText, ctaText, color]);
+
+  return (
+    <div className="bg-slate-100 flex flex-row items-center justify-center justify-evenly w-full h-screen font-poppins">
+      <div>
+        <canvas
+          className="border-4 rounded h-[500px] w-[500px]"
+          ref={canvasRef}
+        ></canvas>
+      </div>
+      <div className="flex flex-col font-poppins">
+        <h1 className="text-center text-[20px] font-bold my-1">Ad customization</h1>
+        <p className="text-center text-[12px] italic mb-3 text-slate-500">Customize your ad and get the templates accordingly</p>
+
+        <div className="relative my-3 mt-5 border-2 border-gray-300 p-3 rounded flex items-center">
+          <label htmlFor="imageUpload" className="text-gray-700 flex items-center mr-2 text-slate-500 text-[14px]">
+            <FontAwesomeIcon icon={faImage} className="mr-2 text-blue-600" />
+            Change the ad creative image:
+          </label>
+          <input
+            id="imageUpload"
+            type="file"
+            accept="image/*"
+            className="hidden"
+            ref={fileInputRef}
+            onChange={handleImageChange}
+          />
+          <a
+            href="#"
+            onClick={triggerInputFile}
+            className="underline text-blue-600 cursor-pointer text-[15px]"
+          >
+            Select File
+          </a>
+        </div>
+        {/*Edit contents */}
+        <div className="container my-5 flex items-center">
+          <div className="flex-grow border-t border-slate-400"></div>
+          <h2 className="title text-sm font-normal text-slate-400 mx-4">Edit contents</h2>
+          <div className="flex-grow border-t border-slate-400"></div>
+        </div>
+
+        <TextField
+          sx={{
+            width: 600,
+            marginTop: 3,
+            marginBottom: 2,
+            border: 1,
+            borderRadius: 2
+          }}
+          id="outlined-basic"
+          className="text-slate-200 margin"
+          label="Ad content"
+          variant="outlined"
+          onChange={(e) => handleCaptionTextChange(e)}
+        />
+        <TextField
+          sx={{
+            width: 600,
+            marginTop: 1,
+            marginBottom: 1,
+            border: 1,
+            borderRadius: 2
+          }}
+          id="outlined-basic"
+          className="text-slate-200"
+          label="CTA"
+          variant="outlined"
+          onChange={(e) => handleCTATextChange(e)}
+        />
+        <p className="text-[14px] font-light italic mt-5 text-slate-500">Choose your color</p>
+        <div className="flex flex-row">
+
+          {/* Color Picker Button */}
+          <div className="relative mr-5 mt-5">
+            <button
+              onClick={() => setShowColorPicker(!showColorPicker)}
+              className="bg-slate-300 border-solid hover:border-dotted rounded-full my-1  px-3 py-1 font-medium text-[20px] cursor-pointer  border hover:border-dotted hover:border-gray-500"
+            >+
+            </button>
+            {/* Color Picker Popover */}
+            {showColorPicker && (
+              <div className="absolute top-10 right-0 left-0 mt-6">
+                <TwitterPicker color={color} onChange={handleColorChange} className="mt-1" />
+              </div>
+            )}
+          </div>
+
+          {/* Display last three selected colors */}
+          <div className="flex items-center mt-5">
+            {lastThreeColors.map((col, index) => (
+              <div
+                key={index}
+                className="rounded-full w-8 h-8 mr-2 cursor-pointer"
+                style={{ backgroundColor: col }}
+                onClick={() => setColor(col)}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
---
+++ src/constants.js
@@ -0,0 +1,35 @@
+export const dummyData = {
+  caption: {
+    text: "1 & 2 BHK Luxury Apartments at just Rs.34.97 Lakhs",
+    position: {
+      x: 50,
+      y: 50,
+    },
+    max_characters_per_line: 31,
+    font_size: 44,
+    alignment: "left",
+    text_color: "#FFFFFF",
+  },
+  cta: {
+    text: "Shop Now",
+    position: {
+      x: 190,
+      y: 320,
+    },
+    text_color: "#FFFFFF",
+    background_color: "#000000",
+  },
+  image_mask: {
+    x: 56,
+    y: 442,
+    width: 970,
+    height: 600,
+  },
+  urls: {
+    mask: "https://d273i1jagfl543.cloudfront.net/templates/global_temp_landscape_temp_10_mask.png",
+    stroke:
+      "https://d273i1jagfl543.cloudfront.net/templates/global_temp_landscape_temp_10_Mask_stroke.png",
+    design_pattern:
+      "https://d273i1jagfl543.cloudfront.net/templates/global_temp_landscape_temp_10_Design_Pattern.png",
+  },
+};
